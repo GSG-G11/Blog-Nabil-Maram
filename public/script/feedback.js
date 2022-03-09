c47da0a7218cb559be6d2b3dc14e042aa885db0d
Maram Ismail <maramismail2015@gmail.com>
2022-03-09 18:34:51 +0200

    edit

--- public/script/feedback.js
+++ public/script/feedback.js
@@ -1,25 +1,24 @@
-const container = document.querySelector('.container');
+const container = document.querySelector(".container");
 const id = window.location.href.split("=")[1];
 
 document.querySelector("form").action = `/feedback/${id}`;
 
 fetch(`/result/${id}`)
-.then(res=>res.json())
-.then(data=> {
+  .then((res) => res.json())
+  .then((data) => {
     document.querySelector(".user-name span").innerText = data[0].name;
-    data.forEach(item=>{
-        const post = document.createElement('div');
-        post.classList.add('all-feedback');
+    data.forEach((item) => {
+      const post = document.createElement("div");
+      post.classList.add("all-feedback");
 
-        const postTitle = document.createElement('p');
-        postTitle.innerText = item.title;
-        post.appendChild(postTitle);
+      const postTitle = document.createElement("p");
+      postTitle.innerText = item.title;
+      post.appendChild(postTitle);
 
-        const postContent = document.createElement('p');
-        postContent.innerText = item.content;
-        post.appendChild(postContent);
+      const postContent = document.createElement("p");
+      postContent.innerText = item.content;
+      post.appendChild(postContent);
 
-        container.appendChild(post);
-    
+      container.appendChild(post);
     });
-})
+  });
