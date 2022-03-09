192382cf3a20734476d8677333818fa234cd3824
Nabil AbuShaban <nabil.abushaban@gmail.com>
2022-03-09 19:47:00 +0200

    Update connection.js

--- source/database/config/connection.js
+++ source/database/config/connection.js
@@ -4,7 +4,7 @@ const {Pool} = require("pg");
 let DB_URL = ""
 
 if(process.env.NODE_ENV === 'production') {
-    DB_URL = process.env.DATABASE_URL
+    DB_URL = process.env.DB
 } else if (process.env.NODE_ENV === 'development') {
     DB_URL = process.env.DEV_DB
 } else if (process.env.NODE_ENV === 'test') {
